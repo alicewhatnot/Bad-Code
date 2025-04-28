a71f41c715635b4e0ad6222ca2d721c2d6766093
Alice <alicegillbanks@gmail.com>
2025-04-28 19:10:41 +0100

    Update 35 Game of Chance.py

--- A-Level Programming Challenges/35 Game of Chance.py
+++ A-Level Programming Challenges/35 Game of Chance.py
@@ -1,41 +1,55 @@
 import random
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-def getInteger(x):
+def getInput(x):
     while True:
-        try:
-            if x == "a":
-                userInput = float(input("Enter your total balance: £"))
-            else:
-                userInput = float(input("Enter your bet: £"))
-                
-            if userInput <= 0:
-                raise Exception
-            return userInput
-        except:
-            print("Invalid entry. Try again")
+        if x== "c":
+            try:
+                userInput = int(input("Enter the number of numbers to bet on."))
+                if userInput < 1 or userInput > 10:
+                    raise Exception
+                return userInput
+            except:
+                print ("Invalid entry. Must be between 1 and 10 numbers.")
+        else:
+            try:
+                if x == "a":
+                    userInput = float(input("Enter your total balance: £"))
+                elif x == "b":
+                    userInput = float(input("Enter your bet: £"))
+                    
+                if userInput <= 0:
+                    raise Exception
+                return userInput
+            except:
+                print("Invalid entry. Try again")
 
-def addBonuses(value):
+def addBonuses(value,amount):
+    numberArray = []
     #Multiplying based on number bonuses
-    number = random.randint(0,30)
     multiplier = 1
     
-    if number % 2 == 0:
-        multiplier = multiplier * 2
-    if number % 10 == 0:
-        multiplier = multiplier * 3
-    if number in primes:
-        multiplier = multiplier * 5
-    if number < 5:
-        multiplier = multiplier * 2
+    for game in range(amount):
+        number = 0
+        while number not in numberArray:
+            number = random.randint(0,30)
+            numberArray.append(number)
+        if number % 2 == 0:
+            multiplier = multiplier * 2
+        if number % 10 == 0:
+            multiplier = multiplier * 3
+        if number in primes:
+            multiplier = multiplier * 5
+        if number < 5:
+            multiplier = multiplier * 2
     
     #Comparing original value to new to check if zero return
     originalValue = value
     value = value * multiplier
     if originalValue == value:
-        value = 0
+        value = -1 * value
         multiplier = 0
-    return value,number,multiplier
+    return value,numberArray,multiplier
 
 def checkBet(account,value):
     return account - value < 0
@@ -44,23 +58,28 @@ def checkZero(account):
     return account == 0
     
 conditions = {"Even": "2x", "Multiple of 10": "3x", "Prime": "5x", "Below 5": "2x"}
-print ("You are betting on a number 0-30")
+print ("You are betting on numbers 0-30")
 for conditions, count in conditions.items():
     print (f"{conditions:<15}: {count}")
 print ("Rewards stack. Good luck!\n")
-balance = getInteger("a")
+balance = getInput("a")
 
 while True:
     if checkZero(balance):
         print ("You have no money left in your account.")
-    bet = getInteger("b")
+        break
+    bet = getInput("b")
     if checkBet(balance,bet):
         print ("Sorry, you don't have enough balance.")
         break
-    bet,number,multiplier = addBonuses(bet)
-    print (f"The number was {number} and you win £{bet} at {multiplier}x.")
+    numBets = getInput("c")
+    bet,numbers,multiplier = addBonuses(bet,numBets)
+    if bet > 0:
+        print (f"The numbers were {numbers} and you win £{bet} at {multiplier}x.")
+    else:
+        print (f"The number was {number} and you loose £{-1*bet}.")
     balance = balance + bet
     print (f"Your current balance is {balance}")
 
-#2. Make it remove money from balance
+#started adding number of bets to the addBonuses function
 #4. Develop your program to allow multiple bets on different numbers
