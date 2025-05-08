1a15c593e455e95373134f635404a799a104669b
Alice <alicegillbanks@gmail.com>
2025-05-08 09:34:41 +0100

    Update 36 Triangulate.py

--- A-Level Programming Challenges/36 Triangulate.py
+++ A-Level Programming Challenges/36 Triangulate.py
@@ -6,14 +6,33 @@ def getInteger(statement : str,version : str):
     while True:
         try:
             userInput : int = int(input(statement))
-            PUT VERSION CONTROL HERE
-            if userInput <= 0:
-                raise Exception
+            if version == "a":
+                if not(userInput == 1 or userInput == 2):
+                    raise Exception
+            else:
+                if userInput <= 0:
+                    raise Exception
             return userInput
         except:
             print("Invalid entry. Try again")
             
-def cosineRule(sideA,sideB,sideC):
+def getFloat(statement : str,version : str):
+    '''Defensive method of asking for a float
+    Can be tweaked for any data type or range'''
+    while True:
+        try:
+            userInput : float = float(input(statement))
+            if version == "a":
+                if not(userInput == 1 or userInput == 2):
+                    raise Exception
+            else:
+                if userInput <= 0:
+                    raise Exception
+            return userInput
+        except:
+            print("Invalid entry. Try again")
+            
+def cosineRuleSide(sideA,sideB,sideC):
     try:
         cosA = (sideA*sideA - sideB*sideB - sideC*sideC)/(-2*sideB*sideC)
         angleA = math.acos(cosA)
@@ -21,21 +40,35 @@ def cosineRule(sideA,sideB,sideC):
     except:
         print ("Invalid triangle.")
         return "ERROR"
+    
+def cosineRuleAngle(angleA,sideB,sideC):
+    try:
+        sideA = math.sqrt(sideB*sideB + sideC*sideC -2*sideB*sideC*math.cos(math.radians(angleA)))
+        return sideA
+    except:
+        print ("Invalid triangle.")
+        return "ERROR"
 
 while True:
-    mode = getInteger("Three sides or two and one angle? (enter a or b): ","a")
-    sideA = getInteger("Enter the first side: ","b")
-    sideB = getInteger("Enter the second side: ","b")
-    sideC = getInteger("Enter the third side: ","b")
-    
-    if cosineRule(sideA,sideB,sideC) == "ERROR":
-        break
-    if sideA == sideB == sideC:
-        print ("Equilateral.")
-    elif cosineRule(sideC,sideA,sideB) == math.pi/2 or cosineRule(sideB,sideC,sideA) == math.pi/2 or cosineRule(sideA,sideB,sideC) == math.pi/2:
-        print ("Right-angled.")
-    else:
-        print ("Scalene")
+    mode = getInteger("Three sides or two and one angle? (enter 1 or 2): ","a")
+    if mode == 1:
         
+        sideA = getInteger("Enter the first side: ","b")
+        sideB = getInteger("Enter the second side: ","b")
+        sideC = getInteger("Enter the third side: ","b")
         
-#develop to take in two sides and an angle, compute missing angle
+        if cosineRuleSide(sideA,sideB,sideC) == "ERROR":
+            break
+        if sideA == sideB == sideC:
+            print ("Equilateral.")
+        elif cosineRuleSide(sideC,sideA,sideB) == math.pi/2 or cosineRuleSide(sideB,sideC,sideA) == math.pi/2 or cosineRuleSide(sideA,sideB,sideC) == math.pi/2:
+            print ("Right-angled.")
+        else:
+            print ("Scalene")
+            
+    if mode == 2:
+        sideB = getInteger("Enter the one of the sides: ","b")
+        sideC = getInteger("Enter the other side: ","b")
+        angleA = getFloat("Enter the angle in degrees: ","b")
+        sideA = cosineRuleAngle(angleA,sideB,sideC)
+        print (f"The third side is {round(sideA,3)} units. (3d.f)")
