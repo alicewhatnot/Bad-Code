ef730a3b04c89a6ede7b91474c67a4c9af0f6a9a
Alice <alicegillbanks@gmail.com>
2025-05-23 13:56:16 +0100

    Update 37 FizzBuzz.py

--- A-Level Programming Challenges/37 FizzBuzz.py
+++ A-Level Programming Challenges/37 FizzBuzz.py
@@ -1,43 +1,50 @@
-Fizz Buzz
-Create a program that replicates the famous game Fizz Buzz. The program will take an input, e.g. 20, and then print out the list of Fizz Buzz up to and including that number, where:
-• Any multiple of 3 is replaced by the word ‘Fizz’
-• Any multiple of 5 is replaced by the word ‘Buzz’
-• Any multiple of both 3 and 5 is replaced by the word ‘FizzBuzz’
-Extension:
-1. Replace any prime number with the word ‘OOPS!’
-2. Allow the user to enter the base numbers that they want to replace words with. E.g. 2 and 3, which would mean:
-• Any multiple of 2 is replaced by the word‘Fizz’
-• Any multiple of 3 is replaced by the word‘Buzz’
-• Any multiple of both 2 and 3 is replaced by the word‘FizzBuzz’
-
-
 class FizzBuzz():
     def __init__(self,userInput):
+        try:
+            self.userInput = userInput 
+            if self.userInput < 0:
+                raise Exception
+        except:
+            print("Invalid entry.")
         self.userInput = userInput
-        
-    def checkInteger(self):
-        #Defensive method of asking for an integer            #put this in the constructor to verify at creation
-        #Can be tweaked for any data type or range
-        while True:
-            try:
-                self.userInput = int(input(self.userInput))
-                if self.userInput < 0:
-                    raise Exception
-                return self.userInput
-            except:
-                print("Invalid entry. Try again")
-        
+        self.fizzed = False
+
     def checkFizz(self):
         if self.userInput % 3 == 0:
-            print ("Fizz")
-        else return False
-            
+            print ("Fizz", end = " ")
+            self.fizzed = True
+        else:
+            return False 
         
     def checkBuzz(self):
         if self.userInput % 5 == 0:
-            print ("Buzz")
-        else return False
+            if self.fizzed == False:
+                print ("Buzz", end = " ")
+            else:
+                print ("Buzz")
+        else:
+            return False
+    
+    def checkOops(self):
+        prime = True
+        for i in range (2, self.userInput):
+            if self.userInput % i == 0:
+                prime = False
+        if prime == True:
+            print ("OOPS")
+        else:
+            return False
+                
+    def outputNumber(self):
+        return self.userInput
     
 #MAIN
-game = FizzBuzz(20)
-game.checkInteger
+game = FizzBuzz(15)
+fizz = False
+buzz = False
+oops = False
+game.checkFizz()
+game.checkBuzz()
+game.checkOops()
+if [fizz,buzz,oops] == False:
+    game.outputNumber()
